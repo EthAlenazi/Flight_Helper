635b8fc759c15cd8664d876c9cc18ebac7c466d6
Atheer <Alenazi.Atheer120@gmail.com>
2024-07-16 22:13:37 +0300

    Clean program file

--- Flight_Helper/WebAPI/Program.cs
+++ Flight_Helper/WebAPI/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-//using Serilog;
 using System.Reflection;
 using System.Text.Json.Serialization;
 using WebAPI.Data;
@@ -13,18 +12,8 @@ using WebAPI.Repository.Implementation;
 using WebAPI.Repository.Interface;
 using WebAPI.Services;
 
-//Log.Logger = new LoggerConfiguration()
-//    .MinimumLevel.Debug()
-//    .WriteTo.Console()
-//    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
-//    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
-//builder.Logging.ClearProviders();
-//builder.Logging.AddConsole();
-//builder.Host.UseSerilog();
-builder.Services.AddProblemDetails();
-// Add services to the container.
 
 builder.Services.AddControllers(
     configure =>
@@ -37,11 +26,6 @@ builder.Services.AddControllers(
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-//builder.Services.AddDbContext<FlightHelperDBContext>(options =>
-//{
-//    options.UseSqlServer(
-//        builder.Configuration["ConnectionStrings:CityConnection"]);
-//});
 var connectionString = builder.Configuration.GetConnectionString("Connection");
 builder.Services.AddDbContext<FlightHelperDBContext>(options =>
     options.UseSqlServer(connectionString));
@@ -54,79 +38,11 @@ builder.Services.AddScoped<IActivityTypeService, ActivityTypeService>();
 builder.Services.AddScoped<ITransportTypeService, TransportTypeService>();
 builder.Services.AddScoped<IAccommodationTypeService, AccommodationTypeService>();
 builder.Services.AddScoped<IUserServices, UserServices>();
-//builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
-//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddApiVersioning(setupAction =>
-{
-    setupAction.ReportApiVersions = true;
-    setupAction.AssumeDefaultVersionWhenUnspecified = true;
-    setupAction.DefaultApiVersion = new ApiVersion(1, 0);
-}).AddMvc().AddApiExplorer(setupAction =>
-{
-    setupAction.SubstituteApiVersionInUrl = true;
-});
-var apiVersionDescriptionProvider = builder.Services.BuildServiceProvider()
-  .GetRequiredService<IApiVersionDescriptionProvider>();
-builder.Services.AddSwaggerGen(
-    setupAction =>
-    {
-        foreach (var description in
-        apiVersionDescriptionProvider.ApiVersionDescriptions)
-        {
-            setupAction.SwaggerDoc(
-                $"{description.GroupName}",
-                new()
-                {
-                    Title = "CityInfo API",
-                    Version = description.ApiVersion.ToString(),
-                    Description = "Through this API you can access cities and their points of interest."
-                });
-        }
-
-        //var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-        //var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
-
-        //setupAction.IncludeXmlComments(xmlCommentsFullPath);
-        //setupAction.AddSecurityDefinition("CityInfoApiBearerAuth", new()
-        //{
-        //    Type = SecuritySchemeType.Http,
-        //    Scheme = "Bearer",
-        //    Description = "Input a valid token to access this API"
-        //});
-
-        setupAction.AddSecurityRequirement(new()
-             {
-                 {
-                     new ()
-                     {
-                         Reference = new OpenApiReference {
-                             Type = ReferenceType.SecurityScheme,
-                             Id = "CityInfoApiBearerAuth" }
-                     },
-                     new List<string>()
-                 }
-             });
-
-
-    }
-    );
 builder.Services.AddResponseCaching();
-//builder.Services.AddHttpCacheHeaders(
-//    (expirationModelOptions) =>
-//    {
-//        expirationModelOptions.MaxAge = 60;
-//        expirationModelOptions.CacheLocation =
-//            Marvin.Cache.Headers.CacheLocation.Private;
-//    },
-//    (validationModelOptions) =>
-//    {
-//        validationModelOptions.MustRevalidate = true;
-//    });
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -153,10 +69,10 @@ if (app.Environment.IsProduction())
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
-//app.UseAuthorization();
+app.UseAuthorization();
 
 app.UseResponseCaching();
-//app.UseHttpCacheHeaders();
+app.UseHttpCacheHeaders();
 app.MapControllers();
 
 app.Run();
